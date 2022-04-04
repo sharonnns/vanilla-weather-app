b6bcd6bb812f382cbe9807d75b9c7da7e9e1f9c2
sharonnns <101915561+sharonnns@users.noreply.github.com>
2022-04-04 16:04:57 +0800

    Removed console log

--- source/index.js
+++ source/index.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let dateElement = document.querySelector("#date");
